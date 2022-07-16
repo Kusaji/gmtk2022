19741a2267a9529216d13d3911785845ac146c6b
Zack <kusajiproduction@gmail.com>
2022-07-16 00:45:52 -0400

    Added footstep sounds

--- GMTK2022/Assets/Scripts/Player/PlayerAudioController.cs
+++ GMTK2022/Assets/Scripts/Player/PlayerAudioController.cs
@@ -10,9 +10,15 @@ public class PlayerAudioController : MonoBehaviour
     public List<AudioClip> attackSounds;
     public List<AudioClip> deathSounds;
     public List<AudioClip> painSounds;
+    public List<AudioClip> footStepSounds;
 
     public void PlayAttackSound()
     {
-        speaker.PlayOneShot(attackSounds[Random.Range(0, attackSounds.Count)], PlayerOptions.soundFXVolume);
+        speaker.PlayOneShot(attackSounds[Random.Range(0, attackSounds.Count)], PlayerOptions.soundFXVolume / 3);
+    }
+
+    public void PlayFootStep()
+    {
+        speaker.PlayOneShot(footStepSounds[Random.Range(0, footStepSounds.Count)], PlayerOptions.soundFXVolume / 3);
     }
 }
